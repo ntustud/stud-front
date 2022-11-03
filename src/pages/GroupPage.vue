c89d9a8f94318046b5282beb19ee09391a6713a8
Dmitry <nasennikdmitrij@gmail.com>
2022-11-03 17:37:22 +0100

    wip: group page is ready

--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -15,17 +15,17 @@
                 </option>
             </select>
             <div class="horizontal-line"></div>
-            <select v-model.number="selectFaculty" class="my-select">
+            <select v-model.number="selectCourse" class="my-select" :disabled="selectFaculty === 0">
                 <option value="0" selected disabled hidden>Курс</option>
-                <option v-for="faculty in faculties" :key="faculty.id" :value="faculty.id" class="my-option">
-                    {{ faculty.name }}
+                <option v-for="option in arrCourses" :key="option.id" :value="option.id" class="my-option">
+                    {{ option.id }}
                 </option>
             </select>
             <div class="horizontal-line"></div>
-            <select v-model.number="selectFaculty" class="my-select">
+            <select v-model.number="selectGroup" class="my-select" :disabled="selectCourse === 0">
                 <option value="0" selected disabled hidden>Група</option>
-                <option v-for="faculty in faculties" :key="faculty.id" :value="faculty.id" class="my-option">
-                    {{ faculty.name }}
+                <option v-for="option in groups" :key="option.id" :value="option.id" class="my-option">
+                    {{ option.name }}
                 </option>
             </select>
         </div>
@@ -34,17 +34,40 @@
   
 <script setup>
 import { useStore } from "vuex";
-import { ref, computed, onMounted } from 'vue';
+import { ref, reactive, computed, onMounted } from 'vue';
 import { UNIVERSITY_ID } from '../../constant';
 import MyButton from '../components/UI/MyButton.vue';
 
 const store = useStore();
 
-const selectFaculty = ref(0);
 const universityID = ref(UNIVERSITY_ID);
+
+const selectFaculty = ref(0);
+const selectCourse = ref(0);
+const selectGroup = ref(0);
+
+let groups = ref({});
+
+const payload = reactive({
+  faculty_id: selectFaculty.value,
+  num_course: selectCourse.value,
+});
+
+const arrCourses = reactive([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]);
 
 const faculties = computed(() => store.state.timeTable.faculties);
 const getFaculties = (universityID) => store.dispatch('timeTable/getFaculties', universityID);
+const getGroupsWhereFacultyAndCourse = (payload) => store.dispatch('timeTable/getGroupsWhereFacultyAndCourse', payload);
+
+async function setGroup() {
+  try {
+    const response = await getGroupsWhereFacultyAndCourse(payload);
+
+    groups.value = response.data.result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 onMounted(() => {
     getFaculties(universityID.value);
