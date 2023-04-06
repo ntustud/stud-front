eb99476e552af828139b4f7983550f8e3c742a84
Dmytro <nasennikdmitrij@gmail.com>
2023-04-07 01:34:45 +0200

    WIP burger menu

---
+++ src/components/BurgerMenu.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="burger-menu" :class="{ 'burder-menu-open': props.open }">
+    <div class="burder-menu__content">
+      <a href="https://t.me/ntustud">Канал</a>
+      <a href="https://t.me/ntustud_bot">Допомога</a>
+    </div>
+  </div>
+</template> 
+  
+<script setup>
+const props = defineProps({
+  open: {
+    type: Boolean,
+    default: false,
+  }
+})
+</script>
+  
+<style scoped lang="scss">
+.burger-menu {
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: 79px;
+  transform: translateY(-120%);
+  height: calc(100vh - 80px);
+  width: 100%;
+  z-index: 1000;
+  background-color: rgba(var(--tg-theme-hint-color-rgb));
+  transition: ease 0.2s;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  overflow: hidden;
+
+  .burder-menu__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 130px;
+
+    a {
+      font-family: 'Montserrat';
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 22px;
+      margin-bottom: 16px;
+    }
+  }
+}
+
+.burder-menu-open {
+  transform: translateY(0);
+}
+</style>
+  
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,105 +1,111 @@
 <template>
-    <header class="header">
-        <div class="wrapper-header wrapper-content">
-            <div class="wrapper-logo">
-                <router-link :to="{ name: 'main' }" class="logo-link">
-                    <IconLogo />
-                </router-link>
-                <div class="verical-line"></div>
-                <p class="header-title">
-                    <slot></slot>
-                </p>
-            </div>
-            <div class="wrapper-navbar">
-                <nav class="navbar">
-                    <a href="https://t.me/ntustud" class="nav-link">Канал</a>
-                    <a href="https://t.me/ntustud_bot">Допомога</a>
-                </nav>
-                <div class="wrapper-burger">
-                    <IconBurger />
-                </div>
-            </div>
+  <header class="header">
+    <div class="wrapper-header wrapper-content">
+      <div class="wrapper-logo">
+        <router-link :to="{ name: 'main' }" class="logo-link">
+          <IconLogo />
+        </router-link>
+        <div class="verical-line"></div>
+        <p class="header-title">
+          <slot></slot>
+        </p>
+      </div>
+      <div class="wrapper-navbar">
+        <nav class="navbar">
+          <a href="https://t.me/ntustud" class="nav-link">Канал</a>
+          <a href="https://t.me/ntustud_bot">Допомога</a>
+        </nav>
+        <div class="wrapper-burger">
+          <IconBurger @click="openBurger = !openBurger" />
         </div>
-    </header>
+        <BurgerMenu :open="openBurger" />
+      </div>
+    </div>
+  </header>
 </template> 
 
 <script setup>
+import { ref } from 'vue';
 import IconLogo from './icons/IconLogo.vue';
 import IconBurger from './icons/IconBurger.vue';
+import BurgerMenu from '@/components/BurgerMenu.vue';
+
+const openBurger = ref(false);
 </script>
 
 <style scoped lang="scss">
 .wrapper-logo {
-    svg {
-        color: var(--tg-theme-button-color);
-    }
+  svg {
+    color: var(--tg-theme-button-color);
+  }
 }
 
 .wrapper-burger {
-    svg {
-        color: var(--tg-theme-text-color);
-    }
+  svg {
+    color: var(--tg-theme-text-color);
+  }
 }
 
 .header {
-    background: rgba(var(--tg-theme-hint-color-rgb), 0.05);
-    padding: 18px 16px 24px;
+  position: relative;
+  background: rgba(var(--tg-theme-hint-color-rgb), 0.05);
+  padding: 18px 16px 24px;
 
-    .wrapper-header {
-        display: flex;
-        flex-direction: row;
-        align-content: center;
-        justify-content: space-between;
-        align-items: center;
+  .wrapper-header {
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    justify-content: space-between;
+    align-items: center;
 
-        .wrapper-logo {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
+    .wrapper-logo {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
 
-            user-select: none;
+      user-select: none;
 
-            .verical-line {
-                width: 1px;
-                height: 20px;
-                background-color: var(--tg-theme-hint-color);
+      .verical-line {
+        width: 1px;
+        height: 20px;
+        background-color: var(--tg-theme-hint-color);
 
-                margin: 0px 8px 0px 12px;
-            }
+        margin: 0px 8px 0px 12px;
+      }
 
-            .header-title {
-                font-family: 'Montserrat', sans-serif;
-                font-size: 18px;
+      .header-title {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 18px;
 
-                color: var(--tg-theme-hint-color);
-            }
-        }
-
-        .wrapper-navbar {
-
-            .navbar {
-                display: none;
-
-                .nav-link {
-                    margin-right: 24px;
-                }
-            }
-
-            .wrapper-burger {
-                user-select: none;
-                cursor: pointer;
-            }
-        }
+        color: var(--tg-theme-hint-color);
+      }
     }
+
+    .wrapper-navbar {
+
+      .navbar {
+        display: none;
+
+        .nav-link {
+          margin-right: 24px;
+        }
+      }
+
+      .wrapper-burger {
+        user-select: none;
+        cursor: pointer;
+      }
+    }
+  }
 }
 
 @media screen and (min-width: 576px) {
-    .navbar {
-        display: block !important;
-    }
+  .navbar {
+    display: block !important;
+  }
 
-    .wrapper-burger {
-        display: none;
-    }
+  .wrapper-burger {
+    display: none;
+  }
 }
 </style>
