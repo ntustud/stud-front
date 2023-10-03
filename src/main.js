1a8e95df37ba8b2013e60064cbe1813485b0b26d
kifrud <tfgdurf@gmail.com>
2023-10-03 09:29:52 +0300

    migration to vite

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { createApp } from 'vue'
-import App from './App'
+import { createApp } from 'vue'; // Import createApp from the Vue package
+import ElementPlus from 'element-plus';
+import App from './App';
 import router from './router/router';
 import store from './store';
-import 'element-plus/dist/index.css'
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 
 app
+  .use(ElementPlus)
   .use(router)
   .use(store)
   .mount('#app');
--- src/store/modules/ScheduleModule.js
+++ src/store/modules/ScheduleModule.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.VUE_APP_API_URL;
+const API_URL = import.meta.env.VITE_APP_API_URL;
 
 const ax = axios.create({
   baseURL: API_URL,
